2648604adf57098cfe794a149d0f8cda61adfaa5
Antonhy14 <107777524+Antonhy14@users.noreply.github.com>
2023-10-05 18:45:19 -0600

    Update Exportaciones.cpp
    3

--- problemas pendientes/Exportaciones.cpp
+++ problemas pendientes/Exportaciones.cpp
@@ -15,95 +15,98 @@ using namespace std;
     'a' = 97 'z' = 122
     '0' = 48 '9' = 57
 */
-void dfs(vector<vector<vector<int>>>& grafo, int node, int dest, vector<bool>& visited, stack<int>& path, vector<stack<int>>& paths) {
-	visited[node]=true;
-	path.push(node);
-    if(node==dest) {
-        paths.pb(path);
-        visited[node]=false;
-        path.pop();
-    }
-	
-	fore(i,1,grafo[node].size()) {
-		if(grafo[node][i].empty()) continue;
-		if(!visited[i]) {
-			dfs(grafo,i,dest,visited,path,paths);
+class Graph{
+	private:
+		int V;
+		vector<vector<vector<int>>> matAd;
+	public:
+		Graph (int v) :V(v) {
+			matAd.resize(V,vector<vector<int>>(V));
 		}
-	}
-	path.pop();
-}
-vector<int> analizePath(vector<vector<vector<int>>>& grafo, int node, int dest) {
-	vector<int> res;
-	
-	
-	stack<int> path;
-	vector<stack<int>> paths;
-	vector<bool> visited(grafo[node].size());
-	dfs(grafo,node,dest,visited,path,paths);
-
-    for(auto& pth: paths) {   
-	    map<int,int> x;
-    	int c=0;
-    	while(!pth.empty()) {
-    		int aux = pth.top();
-    		pth.pop();
-    		
-    		if(pth.empty()) break;
-    		else {
-    		    for(auto prod:grafo[aux][pth.top()]) {
-    				if(x.find(prod)==x.end()) x[prod]=1;
-    				else x[prod]++;
-    		    }
-    		}
-    		c++;
-    	}
-        for(auto cont:x) {
-    		if(cont.second==c) res.pb(cont.first);
-    	}
-    }
-
-	return res;
-}
+		void addEdge(int x, int y, int z) {
+			matAd[x][y].pb(z);
+			matAd[y][x].pb(z);
+		}
+		void dfs(int node, int dest, vector<bool>& visited, stack<int>& path, vector<stack<int>>& paths) {
+			visited[node] = true;
+			path.push(node);
+			
+			if(node==dest) paths.pb(path);
+			
+			fore(i,1,V) {
+				if(matAd[node][i].empty()) continue;
+				if(!visited[i]) {
+					dfs(i, dest, visited, path, paths);
+				}
+			}
+			
+			visited[node] = false;
+			path.pop();
+		}
+		void showRes(set<int>& results) {
+			if(results.empty()) cout << "*";
+			else for(auto& x :results) cout << x << " ";
+			nl;
+		}
+		void calcPath(int src, int dest) {
+			vector<bool> visited(V);
+			vector<stack<int>> paths;
+			stack<int> path;
+			
+			dfs(src, dest, visited, path, paths);
+			set<int> res;
+			
+			for(auto& pth: paths) {
+				map<int,int> prods;
+				int c=0;
+				
+				while(!pth.empty()) {
+					int aux = pth.top();
+					pth.pop();
+					
+					if(pth.empty()) break;
+					else {
+						for(auto x : matAd[aux][pth.top()]) {
+							if(prods.find(x)==prods.end()) prods[x]=1;
+							else prods[x]++;
+						}
+					}
+					c++;
+				}
+				
+				for(auto& p:prods) {
+					if(p.second==c) res.insert(p.first);
+				}
+			}
+			showRes(res);
+		}
+};
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n,m;
-    cin >> n >> m;
-    vector<vector<vector<int>>> grafo(n+1,vector<vector<int>>(n+1));
-    
-    fore(i,0,m) {
-    	int x,y,z;
-    	cin >> x >> y >> z;
-    	grafo[x][y].pb(z);
-    	grafo[y][x].pb(z);
+	
+	int n,m;
+	cin >> n >> m;
+	Graph graph(n+1);
+	
+	fore(i,0,m) {
+		int x,y,z;
+		cin >> x >> y >> z;
+		graph.addEdge(x,y,z);	
 	}
 	
 	int q;
 	cin >> q;
-	vector<vector<int>> res;
 	
 	fore(i,0,q) {
 		int x,y;
 		cin >> x >> y;
-		vector<int> aux;
-		
-		if(!grafo[x][y].empty()) {
-			for(auto z:grafo[x][y]) aux.pb(z);
-		}
-		else {
-			aux=analizePath(grafo,x,y);
-		}
-		
-		res.pb(aux);
+		graph.calcPath(x,y);
 	}
 	
-	for(auto& aux:res) {
-		if(aux.empty()) cout << "*";
-		else for(auto z: aux) cout << z << " ";
-		
-		nl;
-	}
 	
-    return 0;
+	return 0;
 }
+// RTE 15%
+// https://omegaup.com/arena/problem/Exportaciones-ITLAC22/
+// Cambios en la estructura y la comprobación de los productos.
