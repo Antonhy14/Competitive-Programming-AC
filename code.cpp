4e9527ec5c6eb608eac71c6e61ba15d273799b53
Antonhy14 <107777524+Antonhy14@users.noreply.github.com>
2024-02-10 16:24:04 -0600

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ull unsigned long long
-#define ll long long
-#define cnl cout<<"\n"
-#define nl "\n"
+#define ll long long int
+#define cnl cout << '\n'
+#define nl '\n'
 #define sp(x,n) fixed<<setprecision(x) << n
 #define all(x) x.begin(),x.end()
 #define fore(i,l,r) for(auto i=l;i<r;i++)
@@ -19,9 +19,9 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
     
-    
-    
+
     cout << '\n';
     
     return 0;
