84dd21effbee9c96d6f700dc4d53b8bc9dac8e12
Antonhy14 <107777524+Antonhy14@users.noreply.github.com>
2024-03-12 19:33:18 -0600

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ull unsigned long long
 #define ll long long int
@@ -9,16 +9,18 @@ using namespace std;
 #define fore(i,l,r) for(auto i=l;i<r;i++)
 #define pb push_back
 #define pf push_front
+#define lsb(x) (x & -x)
 /*
     Valores ASCII
     'A' = 65 'Z' = 90
     'a' = 97 'z' = 122
     '0' = 48 '9' = 57
 */
+// 1LL *: convierte el siguiente numero a long long int
 int main(){
     cin.tie(0) -> sync_with_stdio(0), cout.tie(0);
 
-	
+    
 
     cout << '\n';
     
