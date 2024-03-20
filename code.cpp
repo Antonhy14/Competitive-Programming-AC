5dfbab6f27fe0fe82f9d47b91967dd8d8ade75b9
Antonhy14 <107777524+Antonhy14@users.noreply.github.com>
2024-03-20 15:30:06 -0600

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -4,8 +4,9 @@ using namespace std;
 #define ll long long int
 #define cnl cout << '\n'
 #define nl '\n'
-#define sp(x,n) fixed<<setprecision(x) << n
+#define sp(x,n) fixed << setprecision(x) << n
 #define all(x) x.begin(),x.end()
+#define sz(x) int(x.size())
 #define fore(i,l,r) for(auto i=l;i<r;i++)
 #define pb push_back
 #define pf push_front
@@ -17,6 +18,8 @@ using namespace std;
     '0' = 48 '9' = 57
 */
 // 1LL *: convierte el siguiente numero a long long int
+const int N = 1e5 + 5;
+
 int main(){
     cin.tie(0) -> sync_with_stdio(0), cout.tie(0);
 
