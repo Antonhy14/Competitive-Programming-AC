6e5764540bf02c9267ca4a2fb4925ab836dab505
Antonhy14 <antonhytorr2@gmail.com>
2024-09-28 10:43:47 -0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,28 +3,28 @@
 using namespace std;
 #define ull unsigned long long
 #define ll long long int
-#define cnl cout << '\n'
+#define ld long double
 #define nl '\n'
+#define cnl cout << '\n'
 #define sp(x,n) fixed << setprecision(x) << n
 #define all(x) x.begin(),x.end()
 #define sz(x) int(x.size())
 #define len(x) int(x.length())
 #define fore(i,l,r) for(auto i=l;i<r;i++)
+#define forex(i,r,l) for(auto i=r-1;i>=l;i--)
+#define lsb(x) (x & -x)
 #define pb push_back
 #define pf push_front
-#define lsb(x) (x & -x)
 
 using vi = vector<int>;
 using vll = vector<ll>;
-using vii = vector<pair<int, int>>;
 using ii = pair<int, int>;
-
-const int N = 1e5 + 5;
+using vii = vector<pair<int, int>>;
 
 int main() {
     cin.tie(0) -> sync_with_stdio(0), cout.tie(0);
 
- 	
+    
 
     return 0;
 }
